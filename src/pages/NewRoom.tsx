baf1cc5035eeff400da89f55f15c7e31f68b5901
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-06-30 03:21:33 -0300

    feat:terminada a tela de criação de sala

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,32 @@
+import IllustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={IllustrationImg} alt="Ilustração simulando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Let me ask" />
+          <h2>Criar uma nova sala</h2>
+          <form action="">
+            <input type="text"
+              placeholder="Nome da sala"            
+            />
+            <Button type="submit">
+              Criar na sala
+            </Button>
+          </form>
+          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+        </div>
+      </main>
+    </div>
+  )
+}
