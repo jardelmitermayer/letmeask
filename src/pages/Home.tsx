26c8b015ef36e27fbcca7cc8d77e795b28a96d2c
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-06-30 03:34:33 -0300

    feat:instalando e usando a biblioteca react-router-dom para navegar entre as telas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router-dom'
+
 import IllustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleImg from '../assets/images/google-icon.svg'
@@ -6,6 +8,13 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
 export function Home () {
+
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push('/rooms/new')
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -16,7 +25,7 @@ export function Home () {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Let me ask" />
-          <button className="create-room">
+          <button className="create-room" onClick={navigateToNewRoom}>
             <img src={googleImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import IllustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 
@@ -24,7 +26,7 @@ export function NewRoom() {
               Criar na sala
             </Button>
           </form>
-          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+          <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
         </div>
       </main>
     </div>
