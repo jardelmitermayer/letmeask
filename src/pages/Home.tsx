87b60b93360ddf69b0111d6a9cde4a99b11f4b49
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-07-02 02:02:50 -0300

    feat:feita a autenticação via firebase na tela home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,23 @@ import logoImg from '../assets/images/logo.svg';
 import googleImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss';
+
 import { Button } from '../components/Button';
+import { auth, firebase } from '../services/firebase';
 
 export function Home () {
 
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new')
+  function handleCreateRoom() {
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then( result => {
+      console.log(result)
+
+      history.push('/rooms/new')
+    });
   }
 
   return (
@@ -25,7 +34,7 @@ export function Home () {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Let me ask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
