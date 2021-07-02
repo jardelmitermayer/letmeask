a2f494241d5d7f999085667089db67bb972806f6
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-07-02 03:11:24 -0300

    feat:componetizando o Auth Context e criando hook para ele

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,17 @@
-import { createContext, useState, useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { auth, firebase } from './services/firebase';
 
-type User = {
-  id: string;
-  name: string;
-  avatar: string
-}
+import { AuthContextProvider } from './contexts/AuthContext'
 
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-}
-
-export const AuthContext = createContext({} as AuthContextType);
-
-function App() {
-  const [user, setUser] = useState<User>();
-
-  useEffect(() => {
-    auth.onAuthStateChanged(user => {
-      if (user) {
-        const { displayName, photoURL, uid } = user
-
-        if (!displayName || !photoURL) {
-          throw new Error('Missing information from Google Account.')
-        }
-
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL
-        })
-      }
-    })
-  }, [])
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider);
-
-    if (result.user) {
-      const { displayName, photoURL, uid } = result.user
-
-      if (!displayName || !photoURL) {
-        throw new Error('Missing information from Google Account.')
-      }
-
-      setUser({
-        id: uid,
-        name: displayName,
-        avatar: photoURL
-      })
-    }
-  }
+function App() { 
   return (
-   <BrowserRouter>
-    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+   <BrowserRouter>   
+    <AuthContextProvider>
       <Route path="/" exact   component={Home} />
       <Route path="/rooms/new" component={NewRoom} />
-      </AuthContext.Provider>
+    </AuthContextProvider>
    </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import IllustrationImg from '../assets/images/illustration.svg'
@@ -8,12 +7,12 @@ import googleImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss';
 
 import { Button } from '../components/Button';
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home () {
 
   const history = useHistory();
-  const { user , signInWithGoogle} = useContext(AuthContext);
+  const { user , signInWithGoogle} = useAuth();
   async function handleCreateRoom() {
     if(!user){
       await signInWithGoogle();
