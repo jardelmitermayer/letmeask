e25935c5c1863e8ae2ae6428e6f353b784c61489
Jardel Mitermayer <jardel.filho@dcomp.ufs.br>
2021-07-12 15:35:55 -0300

    feat:concluido o fluxo de criação de sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FormEvent, useState } from 'react';
 
 import IllustrationImg from '../assets/images/illustration.svg'
@@ -7,16 +7,28 @@ import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 export function NewRoom() {
-  //const { user } = useAuth();
+  const { user } = useAuth();
 
   const [newRoom, setNewRoom] = useState('');
-
+  const history = useHistory();
   async function handleCreateRoom(event: FormEvent) {
     event.preventDefault();
 
-    console.log(newRoom)
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom, 
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`)
   }
 
   return (
